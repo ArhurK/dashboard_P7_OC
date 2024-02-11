cef7e60de2385e78d2f4c0911db8452b08a1d299
ArthurK <arthurromain.kiehl@gmail.com>
2024-02-11 12:09:31 +0100

    upload gitignore and start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Mise à jour des paquets
-apt-get update
+# # Mise à jour des paquets
+# apt-get update
 
-# Installation des dépendances
-apt-get -y install curl
-apt-get install libgomp1
+# # Installation des dépendances
+# apt-get -y install curl
+# apt-get install libgomp1
 
 # lancement de l'application
 streamlit run Dashboard_streamlit.py
