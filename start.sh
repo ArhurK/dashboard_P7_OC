4b70dcd8c4933da16ff8d790c218e153e9690bae
ArthurK <arthurromain.kiehl@gmail.com>
2024-02-12 14:56:10 +0100

    up start

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # # Mise à jour des paquets
-# apt-get update
+apt-get update
 
 # # Installation des dépendances
-# apt-get -y install curl
-# apt-get install libgomp1
+apt-get -y install curl
+apt-get install libgomp1
 
 # lancement de l'application
 python -m streamlit run Dashboard_streamlit.py
