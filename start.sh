fcc4c88b5a66b54fe76aec868925130e12dce83c
ArthurK <arthurromain.kiehl@gmail.com>
2024-02-12 18:55:10 +0100

    up 2 start.sh avec serverport 8000

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ apt-get -y install curl
 apt-get install libgomp1
 
 # lancement de l'application
-python -m streamlit run Dashboard_streamlit.py
+# python -m streamlit run Dashboard_streamlit.py
+python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
